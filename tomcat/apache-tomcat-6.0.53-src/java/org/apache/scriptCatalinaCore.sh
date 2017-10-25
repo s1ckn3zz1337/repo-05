db4b00fdc52c3c5386ce20686d900bb629c5c549
layton736 <1410655@stud.hs-mannheim.de>
2017-10-25 23:26:25 +0200

    add Shell Scripts

---
+++ tomcat/apache-tomcat-6.0.53-src/java/org/apache/scriptCatalinaCore.sh
@@ -0,0 +1,9 @@
+name=catalinaCore
+echo "digraph {" > $name.dot ; grep -R "^import" * | sed -E "s/\//./g" | \
+sed -E "s/\.java//g" | sed -E "s/\.\*//g" | sed -E "s/import //g" | \
+grep -v ":java" | grep -v ":javax" | sed -E "s/;//g" | sed -E "s/:/ -> /" | \
+grep -v "\.properties" | sed -E "s/\./_/g" | sed -E "s/_[A-Z].* ->/ ->/g" | \
+sed -E "s/-> ([a-z_]*)_[A-Z][a-zA-Z]*/-> \1/g" | sed -E "s/org_apache_//g" | \
+grep -v "juli_logging" | grep -v "tomcat_util" | grep " -> catalina_core.*" | \
+ sort | uniq >> $name.dot ; echo "}" >> $name.dot ; \
+dot -Tpdf $name.dot > $name.pdf 
